c919a79515f4b43de7cabc98db6ce6a1e03ee5f2
Mark Wotton <mwotton@gmail.com>
2015-02-08 17:02:57 +0700

    working version

--- src/Text/FastEdit.hs
+++ src/Text/FastEdit.hs
@@ -1,4 +1,53 @@
+{-# LANGUAGE TupleSections #-}
 module Text.FastEdit where
 
-buildDict :: Int -> [String] -> (String -> [(Int, String)])
-buildDict n input = \s -> []
+import           BasePrelude
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.HashMap.Strict        as HM
+import qualified Data.HashSet               as HS
+import qualified Data.Sequence              as Seq
+import           Prelude                    ()
+import           Safe                       (atMay)
+
+cantorise :: (a -> b -> [c]) -> [a] -> [b] -> [c]
+cantorise f as bs = concat $ mapMaybe
+                    (\(ai,bi) -> do
+                        a <- atMay as ai
+                        b <- atMay bs bi
+                        return $ f a b) antiDiagonals
+
+antiDiagonals :: [(Int, Int)]
+antiDiagonals = concatMap anti [0..]
+  where anti n = zip [0..n] (reverse [0..n])
+
+buildDict :: Int -> [String] -> String -> [String]
+buildDict n dictWords = \s -> nub $ cantorise dostuff (deletions s) dicts
+  where
+    dostuff candidates dict = concat $ mapMaybe (`HM.lookup` dict) candidates
+
+    dicts = map (fmap HS.toList . builder) $ transpose $ map (take n . tagged) dictWords
+
+    builder = HM.fromListWith (HS.union) . concatMap foo
+
+    foo (a,as) = map (,HS.singleton a) as
+
+
+tagged s = (map (s,) (deletions s))
+
+deletions :: String -> [[BL.ByteString]]
+deletions s = map (ordNub . deleteN s) [0..(length s)]
+
+deleteN :: String -> Int -> [BL.ByteString]
+deleteN s n = map BL.pack $ toList $ go s n
+  where
+    go :: String -> Int -> Seq.Seq String
+    go s 0 = Seq.singleton s
+    go (x:xs) n = go xs (n-1) <> fmap (x:) (go xs n)
+    go [] _ = Seq.empty
+
+
+ordNub l = go HS.empty l
+  where
+    go _ [] = []
+    go s (x:xs) = if x `HS.member` s then go s xs
+                  else x : go (HS.insert x s) xs
--- test/Text/FastEditSpec.hs
+++ test/Text/FastEditSpec.hs
@@ -6,20 +6,33 @@ import qualified Data.ByteString.Char8 as BS
 import           Data.FileEmbed
 import           Prelude               ()
 import           Test.Hspec
-import           Test.QuickCheck       (property)
+import           Test.QuickCheck
 import           Text.EditDistance
 import           Text.FastEdit
 
 dict = $(embedFile "/usr/share/dict/words")
 
-neighbours = buildDict 3 . map BS.unpack  $ BS.lines dict
+neighbours = buildDict 2 . map BS.unpack  $ BS.lines dict
+
+newtype ReasonableString = ReasonableString String
+  deriving (Show, Eq)
+
+instance Arbitrary ReasonableString where
+  arbitrary = ReasonableString <$> arbitrary `suchThat` (\s -> length s < 10)
 
 spec = describe "fastedit" $ do
   it "has a result on a word" $ do
-    length (neighbours "gold") > 0
+    (`mapM_` ["gold","cake"]) $ \w -> do
+      let n = take 10 $ neighbours w
+      print n
+      (length n > 0) `shouldBe` True
   it "works the same as Text.EditDistance" $ do
+    -- we don't accurately model substitutions. if this behaviour is
+    -- desired, it's probably easier to
 
+    let editCosts = defaultEditCosts { substitutionCosts = ConstantCost 1000 }
     property $
-      \(s::String) ->
-
-        all (\(i, other) -> levenshteinDistance defaultEditCosts other s == i ) (neighbours s)
+      \(ReasonableString s) ->
+        let n = take 3 (neighbours s)
+            distances = map (\x -> (x,levenshteinDistance editCosts s x)) n in
+        (s,distances,n) `shouldBe` (s,sortBy (comparing snd) distances, n)
