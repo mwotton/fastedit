43ce5bbeb5bf2bbcf6253749d5e227f417398119
Mark Wotton <mwotton@gmail.com>
2015-02-16 16:42:15 +0700

    syntax cleanups

--- src/Text/FastEdit.hs
+++ src/Text/FastEdit.hs
@@ -7,6 +7,7 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import           Data.Hashable              (Hashable)
 import qualified Data.HashMap.Strict        as HM
 import qualified Data.HashSet               as HS
+
 import qualified Data.Sequence              as Seq
 import           Prelude                    ()
 import           Safe                       (atMay)
@@ -26,7 +27,7 @@ buildDict :: Int -> [String] -> String -> [String]
 buildDict n dictWords = \s -> nub $ cantorise searchDict (deletions s) dicts
   where
     searchDict candidates dict = concat $ mapMaybe (`HM.lookup` dict) candidates
-    dicts = map (fmap HS.toList . builder) $ transpose $ map (take n . tagged) dictWords
+    dicts = map (fmap HS.toList . builder) . transpose $ map (take n . tagged) dictWords
     builder = HM.fromListWith HS.union . concatMap (\(a,as) -> map (,HS.singleton a) as)
     tagged s = map (s,) (deletions s)
 
@@ -34,10 +35,10 @@ deletions :: String -> [[BL.ByteString]]
 deletions s = map (ordNub . deleteN s) [0..(length s)]
 
 deleteN :: String -> Int -> [BL.ByteString]
-deleteN s n = map BL.pack $ toList $ go s n
+deleteN s = map BL.pack . toList . go s
   where
     go :: String -> Int -> Seq.Seq String
-    go s 0 = Seq.singleton s
+    go x 0 = Seq.singleton x
     go (x:xs) n = go xs (n-1) <> fmap (x:) (go xs n)
     go [] _ = Seq.empty
 
